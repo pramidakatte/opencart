78a9151a93ded62790927fadc18d8099b61e0163
pramidakatte <pramidakatte@gmail.com>
2024-05-20 12:31:04 +0530

    commited

---
+++ operations.java
@@ -0,0 +1,17 @@
+package aithmeticoperations;
+
+public class operations {
+
+	public static void main(String[] args) 
+	{
+		int a=10,b=200,c=300;
+		System.out.println("the sum of a  and b and c is:" +(a+b+c));
+		
+if(a<b)
+	System.out.println("hello world");
+else
+	System.out.println("hii world");
+	
+}
+}
+
---
+++ strings.java
@@ -0,0 +1,16 @@
+package aithmeticoperations;
+import java.lang.String;
+
+public class strings {
+
+	public static void main(String[] args) {
+		String s1="pyramid";
+		String s2="Jethavanam";
+		String str="#################";
+String str2="#################$$$";
+System.out.println(s1+s2);
+System.out.println(str + " %%%%%%% "+ str2);
+System.out.println(s1 +" " + s2  +  " Dhayana ashramam");
+	}
+
+}
